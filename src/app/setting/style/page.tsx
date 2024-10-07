322929907c657c9ec652029ec381315ae6470e50
detourguru <detourguru@gmail.com>
2024-10-07 16:36:52 +0900

    feat: toogle card function added

--- src/app/setting/style/page.tsx
+++ src/app/setting/style/page.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 import Button from "@/componants/button/Button";
 import StyleCard from "@/componants/card/StyleCard";
 import Header from "@/componants/header/Header";
 import { STYLES_DATA } from "@/constants/style";
+import { useState } from "react";
 
 export default function Style() {
   const styles = Object.entries(STYLES_DATA);
+
+  const [select, setSelect] = useState("일상");
+  const handleClick = (key: string) => {
+    setSelect(key);
+  };
 
   return (
     <>
@@ -12,7 +20,13 @@ export default function Style() {
       <section className="grid place-items-center h-full">
         <div className="grid grid-cols-2 gap-5 place-items-center">
           {styles.map((style) => (
-            <StyleCard key={style[0]} style={style[0]} imagePath={style[1]} />
+            <StyleCard
+              key={style[0]}
+              style={style[0]}
+              imagePath={style[1]}
+              isSelected={select === style[0]}
+              onClick={() => handleClick(style[0])}
+            />
           ))}
         </div>
         <Button>저장하기</Button>
--- src/componants/card/StyleCard.tsx
+++ src/componants/card/StyleCard.tsx
@@ -3,11 +3,23 @@ import Image from "next/image";
 interface StyleCardProps {
   style: string;
   imagePath: string;
+  isSelected: boolean;
+  onClick: () => void;
 }
 
-const StyleCard = ({ style, imagePath }: StyleCardProps) => {
+const StyleCard = ({
+  style,
+  imagePath,
+  isSelected,
+  onClick,
+}: StyleCardProps) => {
   return (
-    <button className="relative w-[130px] h-[130px] rounded-xl border">
+    <button
+      onClick={onClick}
+      className={`relative w-[130px] h-[130px] rounded-xl border ${
+        isSelected ? "border-blue-500" : ""
+      }`}
+    >
       <Image width={130} height={130} alt={style} src={imagePath} />
       <span className="absolute bottom-0 inset-x-0 p-2 text-bold-12">
         {style}
