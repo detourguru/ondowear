ff24666939bf8835fa80dd9c97f1821d3453fbb2
detourguru <detourguru@gmail.com>
2024-10-09 17:54:38 +0900

    feat: style save into localstorage

---
+++ src/hooks/useSelectStyle.tsx
@@ -0,0 +1,20 @@
+import { getLocalStorage, setLocalStorage } from "@/app/utils/storage";
+import { useState } from "react";
+
+export const useSelectStyle = () => {
+  let currentStyle = getLocalStorage("style");
+  currentStyle = currentStyle ? JSON.parse(currentStyle).style : "일상";
+  const [select, setSelect] = useState(currentStyle);
+
+  const handleClick = (style: string) => {
+    setSelect(style);
+  };
+
+  const handleSubmit = () => {
+    setLocalStorage("style", { style: select });
+    alert("저장 되었습니다.");
+    location.href = "/setting";
+  };
+
+  return { handleClick, handleSubmit, select };
+};
