901204442ff6719c87d06d158977d856991224ba
detourguru <detourguru@gmail.com>
2024-10-16 13:00:44 +0900

    refactor: metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Ondowear",
+  description: "weather-based outfit recommendation service",
 };
 
 export default function RootLayout({
